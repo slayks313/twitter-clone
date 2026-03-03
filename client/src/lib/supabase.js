e8a2455f4d6649c270d4778fac150e60a903e911
slayks <slayks147@mail.com>
2026-03-03 20:36:10 +0500

    update

--- client/src/lib/supabase.js
+++ client/src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-export const supabase = createClient(
-  "https://alfqimfgcizokvuaowfa.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsZnFpbWZnY2l6b2t2dWFvd2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5OTE0MjgsImV4cCI6MjA4NjU2NzQyOH0.hHtYEMpzJts-7vWGiO5QA_ukhlCdHwZrr2UKfXbqhEE"
-)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
