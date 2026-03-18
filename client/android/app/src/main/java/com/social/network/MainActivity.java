a053fab3705d6e114cfb22a36c82c4f6e4cc3254
slayks <slayks147@mail.com>
2026-03-18 20:11:47 +0500

    update

--- client/android/app/src/main/java/com/social/network/MainActivity.java
+++ client/android/app/src/main/java/com/social/network/MainActivity.java
@@ -2,6 +2,8 @@ package com.social.network;
 
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.CookieManager;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -10,10 +12,22 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // fullscreen
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
+
+        // ✅ включаем cookies (ВАЖНО)
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            cookieManager.setAcceptThirdPartyCookies(
+                    this.bridge.getWebView(),
+                    true
+            );
+        }
     }
 }
